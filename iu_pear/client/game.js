b633d1ccb84707eeda36486a736732db760d1e85
kivenoliva <kivenoliva@gmail.com>
2014-11-20 19:12:49 +0100

    solventado pequeño bug del ultimo commit que no dejaba funcionar la url.Solucionado y funcionando

--- iu_pear/client/game.js
+++ iu_pear/client/game.js
@@ -1,62 +1,62 @@
 var sprites = {
         
-    ccurvo: { sx: 0, sy: 0, w: 64, h: 64, ni:"campo", nc:"campo", nd "campo", ci:"camino", cc:"camino", cd:"campo", 
+    ccurvo: { sx: 0, sy: 0, w: 64, h: 64, ni:"campo", nc:"campo", nd :"campo", ci:"camino", cc:"camino", cd:"campo", 
         si:"campo", sc:"camino", sd:"campo", escudo:false}, //camino de izquierda a abajo
         
-     Cec: { sx: 330, sy: 459, w: 64, h: 64, ni:"ciudad", nc:"ciudad", nd "ciudad", ci:"campo", cc:"ciudad", cd:"campo", 
+     Cec: { sx: 330, sy: 459, w: 64, h: 64, ni:"ciudad", nc:"ciudad", nd: "ciudad", ci:"campo", cc:"ciudad", cd:"campo", 
         si:"ciudad", sc:"ciudad", sd:"ciudad", escudo: false}, //ciudad centro campo a los lados 
         
-     CDiagonal: { sx: 264, sy: 459, w: 64, h: 64, ni:"ciudad", nc:"campo", nd "campo", ci:"ciudad", cc:"campo", cd:"campo", 
+     CDiagonal: { sx: 264, sy: 459, w: 64, h: 64, ni:"ciudad", nc:"campo", nd: "campo", ci:"ciudad", cc:"campo", cd:"campo", 
         si:"ciudad", sc:"ciudad", sd:"ciudad", escudo: false}, //Ciudad diagonal con un campo
         
-     Cce: { sx: 657, sy: 525, w: 64, h: 64, ni:"ciudad", nc:"ciudad", nd "ciudad", ci:"camino", cc:"ciudad", cd:"ciudad", 
+     Cce: { sx: 657, sy: 525, w: 64, h: 64, ni:"ciudad", nc:"ciudad", nd: "ciudad", ci:"camino", cc:"ciudad", cd:"ciudad", 
         si:"ciudad", sc:"ciudad", sd:"ciudad", escudo: true}, //Ciudad a la que le lleva un camino(escudo) 
         
-     Cc: { sx: 133, sy: 591, w: 64, h: 64, ni:"ciudad", nc:"ciudad", nd "ciudad", ci:"camino", cc:"campo", cd:"ciudad", 
+     Cc: { sx: 133, sy: 591, w: 64, h: 64, ni:"ciudad", nc:"ciudad", nd: "ciudad", ci:"camino", cc:"campo", cd:"ciudad", 
         si:"campo", sc:"camino", sd:"ciudad", escudo: false}, //Ciudad que tiene un camino pero el camino no entra en ella 
         //Duda en cc "campo"    Hay que hablarlo con IA, conflicto en el juego
  
-     ce2C: { sx: 788, sy: 66, w: 64, h: 64, ni:"ciudad", nc:"campo", nd "ciudad", ci:"ciudad", cc:"campo", cd:"ciudad", 
+     ce2C: { sx: 788, sy: 66, w: 64, h: 64, ni:"ciudad", nc:"campo", nd: "ciudad", ci:"ciudad", cc:"campo", cd:"ciudad", 
         si:"ciudad", sc:"campo", sd:"ciudad", escudo: false}, //Campo entre dos ciudades
         
-     Ccampo: { sx: 460, sy: 590, w: 64, h: 64, ni:"ciudad", nc:"ciudad", nd "ciudad", ci:"ciudad", cc:"ciudad", cd:"campo", 
+     Ccampo: { sx: 460, sy: 590, w: 64, h: 64, ni:"ciudad", nc:"ciudad", nd: "ciudad", ci:"ciudad", cc:"ciudad", cd:"campo", 
         si:"ciudad", sc:"ciudad", sd:"ciudad", escudo: false}, //Ciudad que tiene casas y un campo a un lado sin camino
         
-     Cladoccentro: { sx: 198, sy: 458, w: 64, h: 64, ni:"campo", nc:"camino", nd "ciudad", ci:"campo", cc:"camino", cd:"ciudad", 
+     Cladoccentro: { sx: 198, sy: 458, w: 64, h: 64, ni:"campo", nc:"camino", nd: "ciudad", ci:"campo", cc:"camino", cd:"ciudad", 
         si:"campo", sc:"camino", sd:"ciudad", escudo: false}, //Ciudad a un lado y en el centro un camino atravesando
         
-     Cccurvo: { sx: 918, sy: 264, w: 64, h: 64, ni:"campo", nc:"campo", nd "ciudad", ci:"camino", cc:"camino", cd:"ciudad", 
+     Cccurvo: { sx: 918, sy: 264, w: 64, h: 64, ni:"campo", nc:"campo", nd: "ciudad", ci:"camino", cc:"camino", cd:"ciudad", 
         si:"campo", sc:"camino", sd:"ciudad", escudo: false}, //Ciudad a un lado y camino curvo sin atravesar
         
-     Ccamporesto: { sx: 65, sy: 195, w: 64, h: 64, ni:"campo", nc:"campo", nd "ciudad", ci:"campo", cc:"campo", cd:"ciudad", 
+     Ccamporesto: { sx: 65, sy: 195, w: 64, h: 64, ni:"campo", nc:"campo", nd: "ciudad", ci:"campo", cc:"campo", cd:"ciudad", 
         si:"ciudad", sc:"ciudad", sd:"ciudad", escudo: false}, //Ciudad a un lado y campo el resto
         
-     Cady: { sx:722 , sy: 525, w: 64, h: 64, ni:"ciudad", nc:"ciudad", nd "ciudad", ci:"ciudad", cc:"campo", cd:"campo", 
+     Cady: { sx:722 , sy: 525, w: 64, h: 64, ni:"ciudad", nc:"ciudad", nd: "ciudad", ci:"ciudad", cc:"campo", cd:"campo", 
         si:"ciudad", sc:"campo", sd:"campo", escudo: false}, //Ciudades adayacentes resto campo
         
-     crucecC: { sx:722 , sy: 394, w: 64, h: 64, ni:"campo", nc:"camino", nd "campo", ci:"camino", cc:"cruce", cd:"camino", 
+     crucecC: { sx:722 , sy: 394, w: 64, h: 64, ni:"campo", nc:"camino", nd: "campo", ci:"camino", cc:"cruce", cd:"camino", 
         si:"ciudad", sc:"ciudad", sd:"ciudad", escudo: false}, //Cruce 3 caminos mas ciudad
         
-     Ccarribadcha: { sx:0 , sy: 64, w: 64, h: 64, ni:"campo", nc:"camino", nd "campo", ci:"campo", cc:"camino", cd:"camino", 
+     Ccarribadcha: { sx:0 , sy: 64, w: 64, h: 64, ni:"campo", nc:"camino", nd: "campo", ci:"campo", cc:"camino", cd:"camino", 
         si:"ciudad", sc:"ciudad", sd:"ciudad", escudo: false}, //Mirando desde ciudad camino hacia arriba y derecha
      
-     m: { sx:526 , sy: 458, w: 64, h: 64, ni:"campo", nc:"campo", nd "campo", ci:"campo", cc:"monasterio", cd:"campo", 
+     m: { sx:526 , sy: 458, w: 64, h: 64, ni:"campo", nc:"campo", nd: "campo", ci:"campo", cc:"monasterio", cd:"campo", 
         si:"campo", sc:"campo", sd:"campo", escudo: false}, //Monasterio
          
-     C: { sx:723 , sy: 65, w: 64, h: 64, ni:"ciudad", nc:"ciudad", nd "ciudad", ci:"ciudad", cc:"ciudad", cd:"ciudad", 
+     C: { sx:723 , sy: 65, w: 64, h: 64, ni:"ciudad", nc:"ciudad", nd: "ciudad", ci:"ciudad", cc:"ciudad", cd:"ciudad", 
         si:"ciudad", sc:"ciudad", sd:"ciudad", escudo: true}, //Todo ciudad con escudo
         
-     cruce4c: { sx:590 , sy: 590, w: 64, h: 64, ni:"campo", nc:"camino", nd "campo", ci:"camino", cc:"cruce", cd:"camino", 
+     cruce4c: { sx:590 , sy: 590, w: 64, h: 64, ni:"campo", nc:"camino", nd: "campo", ci:"camino", cc:"cruce", cd:"camino", 
         si:"campo", sc:"camino", sd:"campo", escudo: false}, //Cruce de 4 caminos con casas en medio
         
-     cruce3c: { sx:786 , sy: 460, w: 64, h: 64, ni:"campo", nc:"camino", nd "campo", ci:"camino", cc:"cruce", cd:"campo", 
+     cruce3c: { sx:786 , sy: 460, w: 64, h: 64, ni:"campo", nc:"camino", nd: "campo", ci:"camino", cc:"cruce", cd:"campo", 
         si:"campo", sc:"camino", sd:"campo", escudo: false}, //Cruce de 3 caminos sin ciudad
         
-     mc: { sx:852 , sy: 460, w: 64, h: 64, ni:"campo", nc:"campo", nd "campo", ci:"campo", cc:"monasterio", cd:"campo", 
+     mc: { sx:852 , sy: 460, w: 64, h: 64, ni:"campo", nc:"campo", nd: "campo", ci:"campo", cc:"monasterio", cd:"campo", 
         si:"campo", sc:"camino", sd:"campo", escudo: false}, //Monasterio con camino
         
-     c: { sx:918 , sy: 394, w: 64, h: 64, ni:"campo", nc:"camino", nd "campo", ci:"campo", cc:"camino", cd:"campo", 
-        si:"campo", sc:"camino", sd:"campo", escudo: false} //Camino recto    
+     c: { sx:918 , sy: 394, w: 64, h: 64, ni:"campo", nc:"camino", nd: "campo", ci:"campo", cc:"camino", cd:"campo", 
+        si:"campo", sc:"camino", sd:"campo", escudo: false}, //Camino recto    
         
         
         
