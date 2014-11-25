44cae356d1f154c3e40d840c0240b18df4a19e7d
vdominguezalcon <v.dominguezalcon@gmail.com>
2014-11-25 19:58:32 +0100

    Arreglado pequeño fallo

--- iu_pear/iu_pear.js
+++ iu_pear/iu_pear.js
@@ -1,3 +1,12 @@
- Accounts.ui.config({
-    passwordSignupFields: 'USERNAME_ONLY'
+
+if (Meteor.isClient) { 
+  Accounts.ui.config({
+ passwordSignupFields: 'USERNAME_ONLY'
  })
+}
+
+if (Meteor.isServer) {
+ Meteor.startup(function () {
+ // code to run on server at startup
+ });
+}
